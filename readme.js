28327398ce17c91674152d9eb16231f785f66956
Camille Durthaller-Renard <camille.durthaller-renard@mines-albi.fr>
2023-07-24 01:49:05 +0700

    chore: deploy my own api to get private repo stats

--- readme.js
+++ readme.js
@@ -35,9 +35,11 @@ I love trying new things and touch everything, from web & mobile development, we
 
 ### My stats 🌐
 
-![](https://github-readme-stats.vercel.app/api?username=camilledtr&theme=dark&hide_border=true&include_all_commits=false&count_private=true)<br/>
+[![wakatime](https://wakatime.com/badge/user/77e2d3cb-9d2f-4ba4-b727-e3eac50804fc.svg?style=for-the-badge)](https://wakatime.com/@77e2d3cb-9d2f-4ba4-b727-e3eac50804fc)
+
+![](https://github-readme-stats-avv0gdc8i-camilledtr.vercel.app/api?username=camilledtr&theme=dark&hide_border=true&include_all_commits=true&count_private=true&hide_title=true)<br/>
 ![](https://github-readme-streak-stats.herokuapp.com/?user=camilledtr&theme=dark&hide_border=true)<br/>
-![](https://github-readme-stats.vercel.app/api/top-langs/?username=camilledtr&theme=dark&hide_border=true&include_all_commits=false&count_private=true&layout=compact)
+![](https://github-readme-stats-avv0gdc8i-camilledtr.vercel.app/api/top-langs/?username=camilledtr&theme=dark&hide_border=true&include_all_commits=false&count_private=true&layout=compact)
 
 #### Visitor count
 
