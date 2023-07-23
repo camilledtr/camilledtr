eaf57d792d1eb2b65786945a4c153039c4821623
Camille Durthaller-Renard <camille.durthaller-renard@mines-albi.fr>
2023-07-23 22:27:52 +0700

    style: add spaces between sections

--- readme.js
+++ readme.js
@@ -4,13 +4,19 @@ Welcome on Cam Codex 🩵 Github profile. I've started learning Web Development 
 
 I love trying new things and touch everything, from web & mobile development, web scraping, to building automation scripts or database models.
 
+### Contact me
+
+[![Gmail](https://img.shields.io/badge/Gmail-D14836?logo=gmail&logoColor=white)](mailto:camilledr10@gmail.com) [![Twitter](https://img.shields.io/badge/Twitter-%231DA1F2.svg?logo=Twitter&logoColor=white)](https://twitter.com/CamCodex) [![Codingame](https://img.shields.io/badge/Codingame-F6C915?logo=codingame&logoColor=black)](https://www.codingame.com/profile/f6e2135424bacf9ed74821de8cf10e906060264)
+
+#
+
 ### My portfolio
 
 [![Portfolio](./assets/portfolio_qr.png/)](https://camilledurthallerrenard.com/)
 
-### Contact me
+##
 
-[![Gmail](https://img.shields.io/badge/Gmail-D14836?logo=gmail&logoColor=white)](mailto:camilledr10@gmail.com) [![Twitter](https://img.shields.io/badge/Twitter-%231DA1F2.svg?logo=Twitter&logoColor=white)](https://twitter.com/CamCodex) [![Codingame](https://img.shields.io/badge/Codingame-F6C915?logo=codingame&logoColor=black)](https://www.codingame.com/profile/f6e2135424bacf9ed74821de8cf10e906060264)
+### My skills
 
 #### Frontend
 
@@ -25,15 +31,19 @@ I love trying new things and touch everything, from web & mobile development, we
 
 ![Figma](https://img.shields.io/badge/figma-%23F24E1E.svg?style=for-the-badge&logo=figma&logoColor=white) ![Ubuntu](https://img.shields.io/badge/Ubuntu-E95420?style=for-the-badge&logo=ubuntu&logoColor=white)
 
+##
+
 ### My stats 🌐
 
 ![](https://github-readme-stats.vercel.app/api?username=camilledtr&theme=dark&hide_border=true&include_all_commits=false&count_private=true)<br/>
 ![](https://github-readme-streak-stats.herokuapp.com/?user=camilledtr&theme=dark&hide_border=true)<br/>
 ![](https://github-readme-stats.vercel.app/api/top-langs/?username=camilledtr&theme=dark&hide_border=true&include_all_commits=false&count_private=true&layout=compact)
 
-### Visitor count
+#### Visitor count
 
 <img src="https://profile-counter.glitch.me/camilledtr/count.svg" />
+
+#
 
 Added on \`23 Jul 2023\`
 
