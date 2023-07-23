f20099a121ab8e79bd214926ecf9d49d2b628283
Camille Durthaller-Renard <camille.durthaller-renard@mines-albi.fr>
2023-07-23 21:28:40 +0700

    init

---
+++ index.js
@@ -0,0 +1,49 @@
+const { promises: fs } = require('fs');
+const readme = require('./readme');
+
+const today = new Date();
+
+function generateNewREADME() {
+  const readmeRow = readme.split('\n');
+
+  function updateIdentifier(identifier, replaceText) {
+    const identifierIndex = findIdentifierIndex(readmeRow, identifier);
+    if (!readmeRow[identifierIndex]) return;
+    readmeRow[identifierIndex] = readmeRow[identifierIndex].replace(
+      `<#${identifier}>`,
+      replaceText
+    );
+  }
+
+  const identifierToUpdate = {
+    today_date: getTodayDate(),
+    signing: getSigning(),
+  };
+
+  Object.entries(identifierToUpdate).forEach(([key, value]) => {
+    updateIdentifier(key, value);
+  });
+
+  return readmeRow.join('\n');
+}
+
+function getSigning() {
+  return `🤖 This README.md is updated automatically each day. \nIt has been inspired by Melvyn's work. Go support his youtube channel: @Melvynx.`;
+}
+
+function getTodayDate() {
+  return today.toDateString();
+}
+
+const findIdentifierIndex = (rows, identifier) =>
+  rows.findIndex((r) => Boolean(r.match(new RegExp(`<#${identifier}>`, 'i'))));
+
+const updateREADMEFile = (text) =>
+  fs.writeFile('./README.md', text, () => console.log(text));
+
+function main() {
+  const newREADME = generateNewREADME();
+  console.log(newREADME);
+  updateREADMEFile(newREADME);
+}
+main();
---
+++ readme.js
@@ -0,0 +1,41 @@
+module.exports = `### Hi there 👋
+
+Welcome on Cam Codex 🩵 Github profile. I've started learning Web Development 2 years ago, but I've been coding for 5 years now.
+
+I love trying new things and touch everything, from web & mobile development, web scraping, to building automation scripts or database models.
+
+### Contact me
+
+[![Gmail](https://img.shields.io/badge/Gmail-D14836?style=for-the-badge&logo=gmail&logoColor=white)](https://twitter.com/CamCodex) [![Twitter](https://img.shields.io/badge/Twitter-%231DA1F2.svg?logo=Twitter&logoColor=white)](https://twitter.com/CamCodex)
+
+[![Codingame](https://img.shields.io/badge/Codingame-F6C915?style=for-the-badge&logo=codingame&logoColor=black)](https://www.codingame.com/profile/f6e2135424bacf9ed74821de8cf10e906060264)
+
+#### Frontend
+
+![Typescript](https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white) ![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB) ![React Native](https://img.shields.io/badge/React_Native-20232A?style=for-the-badge&logo=react&logoColor=61DAFB) ![Bootstrap](https://img.shields.io/badge/Bootstrap-563D7C?style=for-the-badge&logo=bootstrap&logoColor=white)
+
+#### Backend
+
+![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=white) ![Flask](https://img.shields.io/badge/Flask-000000?style=for-the-badge&logo=flask&logoColor=white) ![PHP](https://img.shields.io/badge/PHP-777BB4?style=for-the-badge&logo=php&logoColor=white) ![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)
+![PostgreSQL](https://img.shields.io/badge/PostgreSQL-316192?style=for-the-badge&logo=postgresql&logoColor=white)
+
+#### Others
+
+![Figma](https://img.shields.io/badge/figma-%23F24E1E.svg?style=for-the-badge&logo=figma&logoColor=white) ![Ubuntu](https://img.shields.io/badge/Ubuntu-E95420?style=for-the-badge&logo=ubuntu&logoColor=white)
+
+### My stats 🌐
+
+![](https://github-readme-stats.vercel.app/api?username=camilledtr&theme=dark&hide_border=true&include_all_commits=false&count_private=true)<br/>
+![](https://github-readme-streak-stats.herokuapp.com/?user=camilledtr&theme=dark&hide_border=true)<br/>
+![](https://github-readme-stats.vercel.app/api/top-langs/?username=camilledtr&theme=dark&hide_border=true&include_all_commits=false&count_private=true&layout=compact)
+
+### Visitor count
+
+<img src="https://profile-counter.glitch.me/camilledtr/count.svg" />
+
+Added on \`Sun 23 Jul 2023\`
+
+Last update on <#today_date>
+
+<#signing>
+`;
